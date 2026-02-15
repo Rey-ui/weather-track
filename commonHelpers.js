953fbbc89a2963bf0dc7ff60f0c908271aa3c18c
Rey-ui <112893271+Rey-ui@users.noreply.github.com>
2026-02-15 08:42:10 +0000

    Deploying to gh-pages from @ Rey-ui/weather-track@11e1139d24f948b1fee60b3c289b57b8cd4779ac 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,5 +1,49 @@
-import{a as p,i as u}from"./assets/vendor-db25513e.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))i(r);new MutationObserver(r=>{for(const o of r)if(o.type==="childList")for(const l of o.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&i(l)}).observe(document,{childList:!0,subtree:!0});function s(r){const o={};return r.integrity&&(o.integrity=r.integrity),r.referrerPolicy&&(o.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?o.credentials="include":r.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(r){if(r.ep)return;r.ep=!0;const o=s(r);fetch(r.href,o)}})();const y="169e2d2ec6f9ef07a3c4e60acede402f",g="https://api.openweathermap.org/data/2.5";async function f(e){return(await p.get(`${g}/weather?q=${e}&appid=${y}`)).data}function d(e,t){const s=e.map(({id:i,name:r,main:{temp:o}})=>`<li id="${i}" class="gallery__item">
-                  ${r} <span>${o}</span><a href="../../parcials/city-info.html"></a>
-                  <button class="item-btn">X</button>
-      </li>`).join("");t.insertAdjacentHTML("beforeend",s)}function h({name:e,main:{temp:t}}){return`<li name="${e}" class="gallery__item">${e} <span>${t}</span><a href="../../parcials/city-info.html?name=${e}">hjhj</a></li>`}const C=["London","Tokio","Paris","Berlin","Kyiv","Seoul","Pekin","Toronto"];async function L(e){try{for(let t of C){const s=await f(t);e.insertAdjacentHTML("beforeend",h(s))}}catch(t){console.log(t)}}function S(e){console.log({Clouds:"#icon-cloud"}[e])}function E(e){return JSON.parse(localStorage.getItem(e))||[]}function m(e,t){return localStorage.setItem(t,JSON.stringify(e))}const a="citiesList";S("Clouds");const _=document.querySelector(".our-cities__form"),c=document.querySelector(".our-cities__list"),b=document.querySelector(".popular-cities__list"),v=document.querySelector(".our-cities__clear-btn");let n=E(a)||[];_.addEventListener("submit",w);c.addEventListener("click",T);v.addEventListener("click",$);d(n,c);L(b);async function w(e){e.preventDefault();const t=e.currentTarget,s=t.elements.city.value.trim();if(!s){u.error({title:"Error",message:"❌Sorry, already exist!"});return}try{const i=await f(s);if(console.log(i.weather[0].main),!i||i.length===0)return;{c.innerHTML="";const r=A(i);console.log(n),d(r,c)}}catch(i){console.log(i),u.error({title:"Error",message:"❌Sorry, there are no images matching your search query. Please, try again!"})}finally{t.reset()}}function P(e,t){t.innerHTML="",d(e,t)}function T(e){const t=e.target.closest(".item-btn");if(!t)return;const s=t.closest("li"),i=Number(s.id);n=n.filter(r=>r.id!==i),m(n,a),P(n,c)}function $(){c.innerHTML="",n=[],localStorage.removeItem(a)}function A(e){return n.some(s=>s.id===e.id)?(u.error({title:"Error",message:"❌Sorry, already exist!"}),n):(n=[...n,e],m(n,a),n)}
+import{a as v,i as d}from"./assets/vendor-db25513e.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const n of t)if(n.type==="childList")for(const a of n.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&r(a)}).observe(document,{childList:!0,subtree:!0});function s(t){const n={};return t.integrity&&(n.integrity=t.integrity),t.referrerPolicy&&(n.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?n.credentials="include":t.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function r(t){if(t.ep)return;t.ep=!0;const n=s(t);fetch(t.href,n)}})();const w="169e2d2ec6f9ef07a3c4e60acede402f",_="https://api.openweathermap.org/data/2.5";async function f(i){return(await v.get(`${_}/weather?q=${i}&appid=${w}`)).data}function u(i,e){const s=i.map(({id:r,name:t,main:{humidity:n,temp:a},weather:p,wind:{speed:y}})=>{const h=p[0].main;return`<li id="${r}" class="our-cities__item">
+                  <a href="../../parcials/city-info.html?name=${t}">
+                    <h3>${t}</h3>
+                    <div>
+                      <div>
+                        <div>
+                          <svg class="header__svg" width="35" height="30">
+                              <use href="./img/svg/symbol-defs.svg#${m(h)}"></use>
+                          </svg>
+                          <span>${h}</span>
+                        </div>
+                        <p>${Math.ceil(a-273.15)}°</p>
+                      </div>
+                      <div>
+                          <div>
+                            <svg class="header__svg" width="15" height="15">
+                                <use href="./img/svg/symbol-defs.svg#icon-droplet"></use>
+                            </svg>
+                            <span>${n}%</span>
+                          </div>
+                          <div>
+                            <svg class="header__svg" width="35" height="30">
+                                <use href="./img/svg/symbol-defs.svg#icon-wind"></use>
+                            </svg>
+                            <span>${y}mph</span>
+                          </div>
+                      </div>
+                    </div>
+                  </a>
+                  <button class="item-btn">
+                    <svg class="header__svg" width="35" height="30">
+                      <use href="./img/svg/symbol-defs.svg#icon-trash"></use>
+                    </svg>
+                  </button>
+              </li>`}).join("");e.insertAdjacentHTML("beforeend",s)}function C(i){return JSON.parse(localStorage.getItem(i))||[]}function g(i,e){return localStorage.setItem(e,JSON.stringify(i))}const L=document.querySelector(".our-cities__form"),c=document.querySelector(".our-cities__list"),S=document.querySelector(".popular-cities__list"),b=document.querySelector(".our-cities__clear-btn"),l="citiesList";let o=C(l)||[];u(o,c);function m(i){return{Clear:"icon-sun",Clouds:"icon-cloud",Rain:"icon-cloud-rain",Drizzle:"icon-cloud-drizzle",Thunderstorm:"icon-cloud-lightning",Snow:"icon-cloud-snow",Mist:"icon-weather-windy",Smoke:"icon-weather-windy",Haze:"icon-weather-windy-cloudy",Dust:"icon-triangle",Fog:"icon-eye-off",Sand:"icon-chart-area",Ash:"icon-chart-area",Squall:"icon-warning",Tornado:"icon-warning"}[i]||"icon-cloud"}async function $(i){i.preventDefault();const e=i.currentTarget,s=e.elements.city.value.trim();if(!s){d.error({title:"Error",message:"❌Sorry, already exist!"});return}try{const r=await f(s);if(console.log(r.weather[0].main),!r||r.length===0)return;{c.innerHTML="";const t=A(r);console.log(o),u(t,c)}}catch(r){console.log(r),d.error({title:"Error",message:"❌Sorry, there are no images matching your search query. Please, try again!"})}finally{e.reset()}}function E(i,e){e.innerHTML="",u(i,e)}function T(i){const e=i.target.closest(".item-btn");if(!e)return;const s=e.closest("li"),r=Number(s.id);o=o.filter(t=>t.id!==r),g(o,l),E(o,c)}function M(){c.innerHTML="",o=[],localStorage.removeItem(l)}function A(i){return o.some(s=>s.id===i.id)?(d.error({title:"Error",message:"❌Sorry, already exist!"}),o):(o=[...o,i],g(o,l),o)}function P({id:i,name:e,main:{temp:s},weather:r}){const t=r[0].main;return`<li id="${i}" class="our-cities__item">
+                    <a href="../../parcials/city-info.html?name=${e}">
+                      <h3>${e}</h3>
+                      <div>
+                        <div>
+                          <svg class="header__svg" width="35" height="30">
+                              <use href="./img/svg/symbol-defs.svg#${m(t)}"></use>
+                          </svg>
+                          <span>${t}</span>
+                        </div>
+                        <p>${Math.ceil(s-273.15)}°</p>
+                      </div>
+                    </a>
+                </li>`}const I=["London","Tokio","Paris","Berlin","Kyiv","Seoul","Pekin","Toronto"];async function O(i){try{for(let e of I){const s=await f(e);i.insertAdjacentHTML("beforeend",P(s))}}catch(e){console.log(e)}}L.addEventListener("submit",$);c.addEventListener("click",T);b.addEventListener("click",M);O(S);
 //# sourceMappingURL=commonHelpers.js.map
